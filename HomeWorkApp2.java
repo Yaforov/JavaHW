32cc94c067a1aff88ec1d9b2d220c3c1793f8760
Yaforov <90694570+Yaforov@users.noreply.github.com>
2022-03-25 14:50:37 +0300

    Add new Home Work 2
    Убрал лишние "public"

--- HomeWorkApp2.java
+++ HomeWorkApp2.java
@@ -1,4 +1,3 @@
-
 /**
  * Java. Lesson 1. Homework 2
  * @author Yaforov Roman
@@ -17,7 +16,7 @@ public class HomeWorkApp2 {
     /** Задание №1: Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в риделах
      * от 10 до 20 (включительно), если да - вернуть true, в противном случае - false.
      */
-    public static boolean winthin10and20 (int x1, int x2) {
+     static boolean winthin10and20 (int x1, int x2) {
         int sum = x1 + x2;
         return (10 <= sum && sum <= 20)? true : false;
     }
@@ -26,21 +25,21 @@ public class HomeWorkApp2 {
      * должен напечатать в консоль, положительное ли число передали или отрицательное.
      * Замечание: Ноль - считаем положительным числом.
      */
-    public static void isPositiveOrNegative (int x) {
+     static void isPositiveOrNegative (int x) {
         System.out.println((x >= 0)? "Positive!" : "Negative!");
     }
 
     /** Задание №3: Написать метод, которому в качестве параметра передается целое число.
      * Метод должен вернуть true, если число отрицательное, и false - если положительное.
      */
-    public static boolean isNegative (int x) {
+     static boolean isNegative (int x) {
         return ( x < 0 )? true : false;
     }
 
     /** Задание № 4: Написать метод, которому в качестве аргументов передается строка и число, метод должен отпечатать
      * в консоль указанную строку, указанное количество раз.
      */
-    public static void printWorldNTimes (String world, int times) {
+     static void printWorldNTimes (String world, int times) {
         for (int i = 0; i < times; i++) {
             System.out.println(world);
         }
@@ -50,7 +49,7 @@ public class HomeWorkApp2 {
      *boolean (высокосный - true). Каждый 4-й год является высокосным, кроме каждого 100-го,
      * при этом каждый 400 является высокосным годом.
      */
-    public static boolean checkYear (int y) {
+     static boolean checkYear (int y) {
         return (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)? true : false;
     }
 }
