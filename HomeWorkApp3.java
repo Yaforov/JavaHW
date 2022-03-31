d1a09d6027e5da00172ff8eeb6746e87d61ae968
Yaforov <90694570+Yaforov@users.noreply.github.com>
2022-03-31 11:21:19 +0300

    Исправление в методе lenArrayInitialValue
    Добавлено, чтобы возвращался массив arr

--- HomeWorkApp3.java
+++ HomeWorkApp3.java
@@ -11,7 +11,7 @@ public class HomeWorkApp3 {
         fillArray();
         changeArray();
         fillDiagonal(5);
-        lenArrayInitialValue(5, 10);
+        lenArrayInitialValue(8, 0);
         System.out.println(Arrays.toString(findMinMax(new int[] {2, 5, 5, 8, 6, 8, 11, 9})));
     }
 
@@ -66,11 +66,12 @@ public class HomeWorkApp3 {
     /** Задание №5: Написать метод, принимающий на вход два аргумента: len и initialValue,
      * и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
      */
-    static void lenArrayInitialValue(int len, int initialValue) {
+    static int[] lenArrayInitialValue(int len, int initialValue) {
         int[] arr = new int[len];
         for (int i = 0; i < len; i++)
             arr[i] = initialValue;
         System.out.println(Arrays.toString(arr));
+       return arr;
     }
 
     /** Задание №6:* Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
